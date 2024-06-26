3e5ed11dc088a9c6501cd3ff5eba70e143c9cf7f
Cory Bethune <corybbethune@gmail.com>
2024-06-26 13:58:31 -0700

    Created the registrant class earn_permit method. Moved :permit attribute to attr_accessor. Passes test.

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,15 +1,19 @@
 class Registrant
 
-  attr_reader :name, :age, :permit
-  alias_method :permit?, :permit
+  attr_reader :name, :age
 
-  attr_accessor :license_data
+  attr_accessor :license_data, :permit
+  alias_method :permit?, :permit
 
   def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
     @license_data = {:written=>false, :license=>false, :renewed=>false}
+  end
+
+  def earn_permit
+    @permit = true
   end
 
 end
