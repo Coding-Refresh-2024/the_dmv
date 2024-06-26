14cfb7c568b6c3e85eec7a84c655156e94d36a14
Cory Bethune <corybbethune@gmail.com>
2024-06-26 12:04:23 -0700

    Expanded Facility class to include @registered_vehicle attribute.

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,11 +1,16 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name,
+              :address,
+              :phone,
+              :services,
+              :registered_vehicles
 
   def initialize(hash)
     @name = hash[:name]
     @address = hash[:address]
     @phone = hash[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
