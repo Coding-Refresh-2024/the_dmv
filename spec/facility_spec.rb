b813242465341fa938526cfbb4ef1d8e0f93c13d
Cory Bethune <corybbethune@gmail.com>
2024-06-26 12:43:06 -0700

    Expanded facility test to check for updated registered vehicle plate types.

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -40,5 +42,18 @@ RSpec.describe Facility do
       expect(@cruz.registration_date).to eq(Date.today)
     end
 
+    it "updates the vehicles plate type to one of three types" do
+      expect(@cruz.plate_type).to eq(nil)
+      @facility.register_vehicle(@cruz)
+      expect(@cruz.plate_type).to eq(:regular)
+
+      expect(@bolt.plate_type).to eq(nil)
+      @facility.register_vehicle(@bolt)
+      expect(@bolt.plate_type).to eq(:ev)
+
+      expect(@camaro.plate_type).to eq(nil)
+      @facility.register_vehicle(@camaro)
+      expect(@camaro.plate_type).to eq(:antique)
+    end
   end
 end
