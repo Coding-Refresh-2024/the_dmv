8c51e7c558303a6252feb8317c5ea5c31081dfd6
Cory Bethune <corybbethune@gmail.com>
2024-07-20 10:04:16 -0700

    Created spec test for the FacilityFactory create_facilities method.

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -10,7 +10,23 @@ RSpec.describe FacilityFactory do
 
   describe '#initialize' do
     it "can initalize" do
+      binding.pry
       expect(@facility).to be_an_instance_of(FacilityFactory)
+    end
+  end
+
+  describe '#create_facilities' do
+    it "creates facilities" do
+      ny_fac = @facility.create_facilities(@ny)
+      mo_fac = @facility.create_facilities(@mo)
+
+      expect(ny_fac).to be_an(Array)
+      expect(ny_fac.count).to eq(170)
+      expect(mo_fac).to be_an(Array)
+      expect(mo_fac.count).to eq(178)
+
+
+
     end
   end
 
